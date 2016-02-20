1154a7c72a123d3852f45aaf244a99cd7099b7f2
Ross Harper <ross@rossharper.net>
2016-02-20 20:10:17 +0000

    refactored to parameterized test

--- test/RomanNumeralGeneratorShould.kt
+++ test/RomanNumeralGeneratorShould.kt
@@ -2,17 +2,26 @@
 import com.natpryce.hamkrest.assertion.assertThat
 import com.natpryce.hamkrest.equalTo
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
-class RomanNumeralGeneratorTest {
+@RunWith(Parameterized::class)
+class RomanNumeralGeneratorTest(val arabic: Int, val expectedRoman: String) {
     private var generator: RomanNumeralGenerator = RomanNumeralGenerator()
 
-    @Test
-    fun shouldReturnIfor1() {
-        assertThat(generator.arabicToRoman(1), equalTo("I"));
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data() : Collection<Array<Any>> {
+            return listOf(
+                    arrayOf(1, "I"),
+                    arrayOf(2, "II")
+            )
+        }
     }
 
     @Test
-    fun shouldReturnIIfor2() {
-        assertThat(generator.arabicToRoman(2), equalTo("II"));
+    fun shouldReturnExpectedRomanFortArabic() {
+        assertThat(generator.arabicToRoman(arabic), equalTo(expectedRoman));
     }
 }
