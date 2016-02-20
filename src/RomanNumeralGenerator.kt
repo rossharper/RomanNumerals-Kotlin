248e41d77bd3fdca05d0aa7d0a2662293c76af24
Ross Harper <ross@rossharper.net>
2016-02-20 20:35:49 +0000

    refactor algorithm to mapped values

--- src/RomanNumeralGenerator.kt
+++ src/RomanNumeralGenerator.kt
@@ -1,32 +1,23 @@
 class RomanNumeralGenerator {
-    fun arabicToRoman(arabic: Int): String {
-        var roman = "";
-        var arabicRemaining = arabic
 
-        while(arabicRemaining >= 10) {
-            roman += "X"
-            arabicRemaining -= 10
+    val arabicToRomanMapping : Map<Int, String> = mapOf(
+            Pair(10, "X"),
+            Pair(9, "IX"),
+            Pair(5, "V"),
+            Pair(4, "IV"),
+            Pair(1, "I")
+    )
+
+    fun arabicToRoman(arabicToConvert: Int): String {
+        var arabicRemainder = arabicToConvert
+        var romanOutput = ""
+        arabicToRomanMapping.forEach {
+            val (arabic, roman) = it
+            while(arabicRemainder >= arabic) {
+                romanOutput += roman
+                arabicRemainder -= arabic
+            }
         }
-
-        if(arabicRemaining == 9) {
-            roman += "IX"
-            arabicRemaining -= 9
-        }
-
-        if(arabicRemaining >= 5) {
-            roman += "V"
-            arabicRemaining -= 5
-        }
-
-        if(arabicRemaining == 4) {
-            roman += "IV"
-            arabicRemaining -= 4
-        }
-
-        for(i in arabicRemaining downTo 1) {
-            roman += "I"
-        }
-
-        return roman;
+        return romanOutput
     }
 }
